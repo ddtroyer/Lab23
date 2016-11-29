79475a627b98e5869a377fbb18ca882bd52296b1
Doreen <ddtroyer@gmail.com>
2016-11-29 15:58:30 -0500

    Add Lab 10

---
+++ Lab10Movies/Lab10Movies/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10Movies
+{
+    public class Movie
+    {
+        public Movie(string name, string category)
+        {
+            _name = name;
+            _category = category;
+        }
+        private string _name;
+
+        public string title
+        {
+            get { return _name; }
+        }
+
+        private string _category;
+
+        public string genre
+        {
+            get { return _category; }
+        }
+    }
+}
---
+++ Lab10Movies/Lab10Movies/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answerContinue = null;
+
+            Console.WriteLine("Welcome to the Movie List Application!" + Environment.NewLine);
+            Console.WriteLine("There are 100 movies in this list.");
+
+            do
+            {
+                Console.WriteLine("What category are you interested in?");
+                Console.Write("animated, drama, horror, scifi: ");
+                string genre = Console.ReadLine();
+
+                List<Movie> MovieList = new List<Movie>() { };
+                MovieList = MovieIO.MovieCatalog;
+                MovieList= MovieList.Where(mbox=>mbox.genre==genre).OrderBy(mbox => mbox.title).ToList();
+
+                int i = 0;
+
+                foreach (Movie movie in MovieList)
+                {                
+                    if (movie.genre == genre)
+                    {
+                        i++;
+                        Console.WriteLine(movie.title);
+                        if (i >= 10)
+                        break;
+                    }              
+                }
+                Console.Write("Continue? (y/n)");
+                answerContinue = Console.ReadLine();
+            }
+            while (answerContinue == "y");
+        }
+    }
+}
+
+

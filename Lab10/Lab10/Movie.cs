e45ad6d3113765ab42b9baa29cd64df3c8021f3a
MichaelAudu <michael.audu@aya.yale.edu>
2016-11-29 15:46:48 -0500

    Michael's Initial Commit

---
+++ Lab10/Lab10/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Movie
+    {
+        public Movie(string name, string category)
+        {
+            this.name = name;
+            this.Category = category;
+        }
+        internal string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        internal string category;
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Lab10/Lab10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Movie> MoviesList = MovieIO.MovieCatalog;
+            bool continueOn;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Enter a Category:/n");
+                Console.WriteLine("animated\ndrama\nhorror\nmusical\nscifi");
+
+                string category = Console.ReadLine();
+                MoviesList = MoviesList.OrderBy(m => m.Name).ToList();
+                foreach (Movie m in MoviesList)
+                {
+                    if (m.Category == category)
+                    {
+                        Console.WriteLine($"Title: {m.name}, Category: {m.category}");
+                    }
+                }
+
+                Console.WriteLine("Continue (Y/N)?");
+                string answer = Console.ReadLine().ToLower();
+                continueOn = answer == "y" ? true : false;
+            } while (continueOn);
+
+        }
+    }
+}
